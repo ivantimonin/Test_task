d4f0ba57f7c80aa8027cb387a9c0b3a90f4da37c
timon <timon@DESKTOP-ANCORQQ>
2020-03-26 20:50:22 +0300

    Первый вариант

--- Test_task/Program.cs
+++ Test_task/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ds.test.impl;
 
 namespace Test_task
 {
@@ -10,6 +11,18 @@ namespace Test_task
     {
         static void Main(string[] args)
         {
+            Plugins obj = new Plugins(10);//все что доступно классу
+            IPlugin new_obj = new Plugins();//все что доступно интерфейсу
+                        
+            obj.WriteNewStar("Альфа Центавра");
+            obj.WriteNewStar("а Большого Пса");
+            obj.Read_all_Star();
+            
+            int rezult = obj.Run(1, 6);
+            Console.WriteLine($"Результат равен {rezult} ");           
+            Console.ReadLine();
         }
     }
+    
+
 }
